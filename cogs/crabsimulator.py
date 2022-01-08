d5090628d3de5e77583850d0faf346f8d6689e8b
Samuel <33796679+orchidalloy@users.noreply.github.com>
2022-01-08 01:29:11 -0300

    implement conversations

--- cogs/crabsimulator.py
+++ cogs/crabsimulator.py
@@ -5,17 +5,27 @@ import json
 from typing import List
 from discord.ext import commands
 
+GUILD_ID = 756395872811483177
+CHANNEL_ID = 929193206472671262
+WEBHOOK_NAME = "CrabSimulator"
+MESSAGE_DELAY = 1
+MESSAGE_CHANCE = 10
+CONVERSATION_DELAY = 60
+CONVERSATION_CHANCE = 30
+CONVERSATION_MIN = 3
+CONVERSATION_MAX = 20
+
+def one_in(chance: int) -> bool:
+    return random.randrange(0, chance) == 0
+
 class Crab:
     def __init__(self, user_id: int, phrases: List[str]):
         self.user_id = user_id
         self.phrases = phrases
         self.user: discord.User = None
 
-class Simulator(commands.Cog):
-    GUILD_ID = 756395872811483177
-    CHANNEL_ID = 929193206472671262
-    WEBHOOK_NAME = "CrabSimulator"
 
+class Simulator(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.running = False
@@ -23,6 +33,7 @@ class Simulator(commands.Cog):
         self.channel: discord.TextChannel = None
         self.webhook: discord.Webhook = None
         self.crabs: List[Crab] = []
+        self.conversation_left = 0
         self.last_phrase: str = ""
         if self.bot.is_ready():
             asyncio.create_task(self.on_ready())
@@ -36,18 +47,23 @@ class Simulator(commands.Cog):
             await self.setup()
             await self.run()
 
+    @commands.command()
+    async def trigger(self, ctx):
+        """Trigger a crab simulator conversation"""
+        self.conversation_left = random.randrange(CONVERSATION_MIN, CONVERSATION_MAX + 1)
+        await ctx.message.add_reaction('✅')
+
     async def setup(self):
-        self.guild = self.bot.get_guild(self.GUILD_ID)
-        self.channel = self.guild.get_channel(self.CHANNEL_ID)
+        self.guild = self.bot.get_guild(GUILD_ID)
+        self.channel = self.guild.get_channel(CHANNEL_ID)
         try:
             # webhook
             webhooks: List[discord.Webhook] = await self.channel.webhooks()
             for webhook in webhooks:
-                if webhook.name == self.WEBHOOK_NAME:
+                if webhook.name == WEBHOOK_NAME:
                     self.webhook = webhook
             if self.webhook is None:
-                self.webhook = await self.channel.create_webhook(name=self.WEBHOOK_NAME)
-
+                self.webhook = await self.channel.create_webhook(name=WEBHOOK_NAME)
             # users
             with open('crabs.json') as f:
                 data: dict = json.load(f)
@@ -64,16 +80,22 @@ class Simulator(commands.Cog):
     async def run(self):
         self.running = True
         while self.running:
-            await asyncio.sleep(1)
-            if random.randint(0, 10) == 0:
-                try:
-                    await self.send()
-                except Exception as error:
-                    print(error)
+            if self.conversation_left:
+                if one_in(MESSAGE_CHANCE):
                     try:
-                        await self.channel.send(f'Error: {error}')
-                    except:
-                        pass
+                        await self.send()
+                        self.conversation_left -= 1
+                    except Exception as error:
+                        print(error)
+                        try:
+                            await self.channel.send(f'Error: {error}')
+                        except:
+                            pass
+                await asyncio.sleep(MESSAGE_DELAY)
+            else:
+                if one_in(CONVERSATION_CHANCE):
+                    self.conversation_left = random.randrange(CONVERSATION_MIN, CONVERSATION_MAX + 1)
+                await asyncio.sleep(CONVERSATION_DELAY)
 
     async def send(self):
         crab = random.choice(self.crabs)
