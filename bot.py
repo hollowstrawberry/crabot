2554bab5fbe7d91bffd0510699f32484ff49c9e6
Samuel <33796679+orchidalloy@users.noreply.github.com>
2022-01-11 15:09:27 -0300

    prefix

--- bot.py
+++ bot.py
@@ -4,10 +4,13 @@ import json
 import os
 from discord.ext import commands
 
-prefix = 'crab'
+DEFAULTPREFIX = '%'
+
 def get_prefix(bot: commands.Bot, message):
-    m = re.match(fr'^({prefix}|<@!?{bot.user.id}>),? ?', message.content, re.IGNORECASE)
-    return m.group() if m else prefix
+    try:
+        return m.group() if (m := bot.prefix.match(message.content)) else DEFAULTPREFIX
+    except Exception:
+        return DEFAULTPREFIX
 
 intents = discord.Intents(members=True, guilds=True, guild_messages=True)
 bot = commands.Bot(command_prefix=get_prefix, activity=discord.Game('Minecraft'), intents=intents)
@@ -17,6 +20,7 @@ with open('token.json') as f:
 
 @bot.event
 async def on_ready():
+    bot.prefix = re.compile(fr'^({DEFAULTPREFIX}|crab(ot)?|<@!?{bot.user.id}>),? ?', re.IGNORECASE)
     print('Ready')
 
 for filename in os.listdir('./cogs'):
